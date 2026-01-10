8dc43257b84f0ceb381e3050c8e98ddd1406abb9
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-10 23:45:08 +0200

    Add OpenAPI documentation with drf-spectacular integration

--- crm_users/urls.py
+++ crm_users/urls.py
@@ -1,9 +1,21 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView, SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
     path("api/auth/", include("apps.users.urls", namespace="user")),
 ]
 
