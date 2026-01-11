700c4abdb4c3f5fee1f6754b86c9f23df219ee31
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-11 21:17:19 +0200

    Update settings to include

--- crm_users/settings.py
+++ crm_users/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False) == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
 
 
 # Application definition
